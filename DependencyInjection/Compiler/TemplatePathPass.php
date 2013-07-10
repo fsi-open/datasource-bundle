75b726c6d81ade3bdbc070466ee3b46e56c4d324
Lukasz Cybula <lukasz.cybula@fsi.pl>
2013-07-10 08:00:22 +0200

    Unified php files' docblocks

--- DependencyInjection/Compiler/TemplatePathPass.php
+++ DependencyInjection/Compiler/TemplatePathPass.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * (c) Fabryka Stron Internetowych sp. z o.o <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FSi\Bundle\DataSourceBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
