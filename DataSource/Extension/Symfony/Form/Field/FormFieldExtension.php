23bfa86b93523f0de2a622373980aa6276714f7d
Lukasz Cybula <lukasz@fsi.pl>
2013-11-15 12:37:44 +0100

    Added default translated choices to isNull comparison and boolean field type

--- DataSource/Extension/Symfony/Form/Field/FormFieldExtension.php
+++ DataSource/Extension/Symfony/Form/Field/FormFieldExtension.php
@@ -11,9 +11,23 @@ namespace FSi\Bundle\DataSourceBundle\DataSource\Extension\Symfony\Form\Field;
 
 use FSi\Component\DataSource\Field\FieldTypeInterface;
 use FSi\Component\DataSource\Field\FieldAbstractExtension;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class FormFieldExtension extends FieldAbstractExtension
 {
+    /**
+     * @var \Symfony\Component\Translation\TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @param \Symfony\Component\Translation\TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -27,13 +41,26 @@ class FormFieldExtension extends FieldAbstractExtension
      */
     public function initOptions(FieldTypeInterface $field)
     {
-        $field->getOptionsResolver()
-            ->setDefaults(array(
-                'form_null_value' => 'datasource.form.choices.is_null',
-                'form_not_null_value' => 'datasource.form.choices.is_not_null',
-                'form_true_value' => 'datasource.form.choices.yes',
-                'form_false_value' => 'datasource.form.choices.no',
-                'form_translation_domain' => 'DataSourceBundle'
-            ));
+        if ($field->getComparison() == 'isNull') {
+            $field->getOptionsResolver()
+                ->setDefaults(array(
+                    'form_options' => array(
+                        'choices' => array(
+                            'null' => $this->translator->trans('datasource.form.choices.is_null', array(), 'DataSourceBundle'),
+                            'no_null' => $this->translator->trans('datasource.form.choices.is_not_null', array(), 'DataSourceBundle')
+                        )
+                    )
+                ));
+        } else if ($field->getType() == 'boolean') {
+            $field->getOptionsResolver()
+                ->setDefaults(array(
+                    'form_options' => array(
+                        'choices' => array(
+                            '1' => $this->translator->trans('datasource.form.choices.yes', array(), 'DataSourceBundle'),
+                            '0' => $this->translator->trans('datasource.form.choices.no', array(), 'DataSourceBundle')
+                        )
+                    )
+                ));
+        }
     }
 }
